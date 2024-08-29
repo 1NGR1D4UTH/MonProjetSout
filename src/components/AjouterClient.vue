c56e4d35ec59885f00684b1904cdd7c7346d471f
Dimitri237 <123161517+Dimitri237@users.noreply.github.com>
2024-08-29 13:51:33 +0100

    update

--- src/components/AjouterClient.vue
+++ src/components/AjouterClient.vue
@@ -11,17 +11,18 @@
       <div class="input-field">
         <div> <label for="name">Nom et Prénom </label></div>
         <div class="form-row">
-        <input class="input" type="text" id="addClientusername" v-model="nom" required>
-        <input class="input" type="text" id="addClientsurname" v-model="prenom" required>
+          <input class="input" type="text" id="addClientusername" v-model="nom" required>
+          <input class="input" type="text" id="addClientsurname" v-model="prenom" required>
+        </div>
       </div>
-      </div>
-      
+
       <div class="input-field">
-          <div> <label for="name">Date et Lieu de naissance</label></div>
-          <div class="form-row">
-            <input class="input" type="date" id="addClientbirthday" v-model="date_naissance" required>
-            <input class="input" type="text" id="addClientlieuNaissance" v-model="lieu_naissance" placeholder="A" required>
-          </div>
+        <div> <label for="name">Date et Lieu de naissance</label></div>
+        <div class="form-row">
+          <input class="input" type="date" id="addClientbirthday" v-model="date_naissance" required>
+          <input class="input" type="text" id="addClientlieuNaissance" v-model="lieu_naissance" placeholder="A"
+            required>
+        </div>
       </div>
       <div class="input-field">
         <div> <label for="name">Sexe</label></div>
@@ -37,11 +38,11 @@
       <div class="input-field">
         <div> <label for="telephone">Adresse email et Numéro de téléphone </label></div>
         <div class="form-row">
-        <input class="input" id="addClientemail" v-model="email" placeholder="Email" required>
-        <input class="input" id="addClientphone" v-model="telephone"  required>
+          <input class="input" id="addClientemail" v-model="email" placeholder="Email" required>
+          <input class="input" id="addClientphone" v-model="telephone" required>
+        </div>
       </div>
-      </div>
-      
+
       <div class="input-field">
         <div> <label for="name">Localisation</label></div>
         <input class="input" type="text" id="addClientlocalisation" v-model="localisation" required>
@@ -57,7 +58,7 @@
         <span>Ajouter</span>
       </button>
     </form>
-    
+
   </div>
 </template>
 
@@ -68,14 +69,14 @@ export default {
     return {
       nom: '',
       prenom: '',
-      date_naissance: '',
+      date_naissance: null,
       lieu_naissance: '',
       sexe: '',
       telephone: '',
       email: '',
       localisation: '',
       mot_de_passe: '',
-      create_by: '', 
+      create_by: '',
       successMessage: '',
       errorMessage: ''
     };
@@ -83,26 +84,32 @@ export default {
   mounted() {
   },
   methods: {
-    
     async addClient() {
       try {
+        // Convertir date_naissance en objet Date si ce n'est pas déjà fait
+        const dateNaissance = new Date(this.date_naissance);
+
+        // Vérifiez si la date est valide
+        if (isNaN(dateNaissance.getTime())) {
+          throw new Error('Date de naissance invalide');
+        }
+
         const response = await axios.post('http://localhost:3000/addClient', {
           create_by: localStorage.getItem('userId'),
           nom: this.nom,
           sexe: this.sexe,
           prenom: this.prenom,
-          date_naissance: new Date(this.date_naissance),
+          date_naissance: dateNaissance.toISOString().split('T')[0], // Format YYYY-MM-DD
           telephone: this.telephone,
           email: this.email,
           localisation: this.localisation,
           lieu_naissance: this.lieu_naissance,
           mot_de_passe: this.mot_de_passe
-        }, {
         });
 
         console.log(response.data.message);
       } catch (error) {
-          console.log('Echec de l\'ajout du client');
+        console.log('Echec de l\'ajout du client', error.response ? error.response.data : error.message);
       }
     }
   }
@@ -122,11 +129,14 @@ h2 {
 }
 
 .form-row {
-display: flex;
-gap: 10px; /* Espace entre les champs */
+  display: flex;
+  gap: 10px;
+  /* Espace entre les champs */
 }
+
 .input {
-flex: 1; /* Pour que les champs prennent la même largeur */
+  flex: 1;
+  /* Pour que les champs prennent la même largeur */
 }
 
 .stext {
@@ -200,7 +210,7 @@ label {
   border-radius: 10px;
   height: 35px;
   color: white;
-left: 100%;
+  left: 100%;
 }
 
 /*.pas {
